31472af7c750f75eb02bb26bf1add5517de5e870
Exel <53962638+mediocreasian@users.noreply.github.com>
2025-07-14 13:47:49 +0800

    Updated Annotation Import

--- src/main/java/com/example/keycloak/modules/user/controller/UserController.java
+++ src/main/java/com/example/keycloak/modules/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.keycloak.modules.user.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
